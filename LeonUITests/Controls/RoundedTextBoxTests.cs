08856e8db2129c6fa841898f3cbfb39f60d582ec
Leon <xiaoyankeji@hotmail.com>
2017-12-12 12:51:30 +0800

    优化RoundedTextBox边框

---
+++ LeonUITests/Controls/RoundedTextBoxTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeonUI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeonUI.Controls.Tests
+{
+    [TestClass()]
+    public class RoundedTextBoxTests
+    {
+        [TestMethod()]
+        public void RoundedTextBoxTest()
+        {
+            Form form = new Form();
+            form.Controls.Add(new RoundedTextBox() { Location=new System.Drawing.Point(0,0)});
+            form.ShowDialog();
+        }
+    }
+}
