58bcd49f7587f639a4f1688cbb8367fced5efb13
Leon <xiaoyankeji@hotmail.com>
2018-03-14 10:14:30 +0800

    增加一个演示项目；

---
+++ Demo/DemoForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Demo
+{
+    partial class DemoForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DemoForm));
+            this.bigButton1 = new LeonUI.Controls.BigButton();
+            this.largeButton1 = new LeonUI.Controls.LargeButton();
+            this.roundedCheckBox1 = new LeonUI.Controls.RoundedCheckBox();
+            this.roundedButton1 = new LeonUI.Controls.RoundedButton();
+            this.roundedButton2 = new LeonUI.Controls.RoundedButton();
+            this.roundedButton3 = new LeonUI.Controls.RoundedButton();
+            this.SuspendLayout();
+            // 
+            // bigButton1
+            // 
+            this.bigButton1.AutoEllipsis = true;
+            this.bigButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bigButton1.ForeColor = System.Drawing.Color.White;
+            this.bigButton1.Image = ((System.Drawing.Image)(resources.GetObject("bigButton1.Image")));
+            this.bigButton1.Location = new System.Drawing.Point(12, 9);
+            this.bigButton1.MaximumSize = new System.Drawing.Size(168, 54);
+            this.bigButton1.MinimumSize = new System.Drawing.Size(168, 54);
+            this.bigButton1.Name = "bigButton1";
+            this.bigButton1.Size = new System.Drawing.Size(168, 54);
+            this.bigButton1.TabIndex = 0;
+            this.bigButton1.Text = "bigButton1";
+            this.bigButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // largeButton1
+            // 
+            this.largeButton1.AutoEllipsis = true;
+            this.largeButton1.BackColor = System.Drawing.Color.Transparent;
+            this.largeButton1.ForeColor = System.Drawing.Color.White;
+            this.largeButton1.Image = ((System.Drawing.Image)(resources.GetObject("largeButton1.Image")));
+            this.largeButton1.Location = new System.Drawing.Point(186, 9);
+            this.largeButton1.MaximumSize = new System.Drawing.Size(308, 113);
+            this.largeButton1.MinimumSize = new System.Drawing.Size(308, 113);
+            this.largeButton1.Name = "largeButton1";
+            this.largeButton1.Size = new System.Drawing.Size(308, 113);
+            this.largeButton1.TabIndex = 1;
+            this.largeButton1.Text = "largeButton1";
+            this.largeButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // roundedCheckBox1
+            // 
+            this.roundedCheckBox1.BackColor = System.Drawing.Color.Transparent;
+            this.roundedCheckBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("roundedCheckBox1.BackgroundImage")));
+            this.roundedCheckBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.roundedCheckBox1.Checked = false;
+            this.roundedCheckBox1.Location = new System.Drawing.Point(497, 9);
+            this.roundedCheckBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.roundedCheckBox1.MaximumSize = new System.Drawing.Size(204, 64);
+            this.roundedCheckBox1.MinimumSize = new System.Drawing.Size(50, 15);
+            this.roundedCheckBox1.Name = "roundedCheckBox1";
+            this.roundedCheckBox1.Size = new System.Drawing.Size(102, 32);
+            this.roundedCheckBox1.TabIndex = 2;
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.AutoEllipsis = true;
+            this.roundedButton1.BackColor = System.Drawing.Color.Transparent;
+            this.roundedButton1.CenterRectangle = new System.Drawing.Rectangle(17, 16, 70, 2);
+            this.roundedButton1.DownBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton1.DownBitmap")));
+            this.roundedButton1.EnterBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton1.EnterBitmap")));
+            this.roundedButton1.ForeColor = System.Drawing.Color.White;
+            this.roundedButton1.Image = ((System.Drawing.Image)(resources.GetObject("roundedButton1.Image")));
+            this.roundedButton1.Location = new System.Drawing.Point(12, 73);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.NormalBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton1.NormalBitmap")));
+            this.roundedButton1.Size = new System.Drawing.Size(126, 49);
+            this.roundedButton1.TabIndex = 3;
+            this.roundedButton1.Text = "roundedButton1";
+            this.roundedButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // roundedButton2
+            // 
+            this.roundedButton2.AutoEllipsis = true;
+            this.roundedButton2.BackColor = System.Drawing.Color.Transparent;
+            this.roundedButton2.CenterRectangle = new System.Drawing.Rectangle(17, 16, 70, 2);
+            this.roundedButton2.DownBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton2.DownBitmap")));
+            this.roundedButton2.EnterBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton2.EnterBitmap")));
+            this.roundedButton2.ForeColor = System.Drawing.Color.White;
+            this.roundedButton2.Image = ((System.Drawing.Image)(resources.GetObject("roundedButton2.Image")));
+            this.roundedButton2.Location = new System.Drawing.Point(12, 392);
+            this.roundedButton2.Name = "roundedButton2";
+            this.roundedButton2.NormalBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton2.NormalBitmap")));
+            this.roundedButton2.Size = new System.Drawing.Size(126, 49);
+            this.roundedButton2.TabIndex = 4;
+            this.roundedButton2.Text = "roundedButton2";
+            this.roundedButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // roundedButton3
+            // 
+            this.roundedButton3.AutoEllipsis = true;
+            this.roundedButton3.BackColor = System.Drawing.Color.Transparent;
+            this.roundedButton3.CenterRectangle = new System.Drawing.Rectangle(17, 16, 70, 2);
+            this.roundedButton3.DownBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton3.DownBitmap")));
+            this.roundedButton3.EnterBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton3.EnterBitmap")));
+            this.roundedButton3.ForeColor = System.Drawing.Color.White;
+            this.roundedButton3.Image = ((System.Drawing.Image)(resources.GetObject("roundedButton3.Image")));
+            this.roundedButton3.Location = new System.Drawing.Point(144, 392);
+            this.roundedButton3.Name = "roundedButton3";
+            this.roundedButton3.NormalBitmap = ((System.Drawing.Bitmap)(resources.GetObject("roundedButton3.NormalBitmap")));
+            this.roundedButton3.Size = new System.Drawing.Size(126, 49);
+            this.roundedButton3.TabIndex = 5;
+            this.roundedButton3.Text = "roundedButton3";
+            this.roundedButton3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.roundedButton3);
+            this.Controls.Add(this.roundedButton2);
+            this.Controls.Add(this.roundedButton1);
+            this.Controls.Add(this.roundedCheckBox1);
+            this.Controls.Add(this.largeButton1);
+            this.Controls.Add(this.bigButton1);
+            this.Name = "DemoForm";
+            this.Text = "Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LeonUI.Controls.BigButton bigButton1;
+        private LeonUI.Controls.LargeButton largeButton1;
+        private LeonUI.Controls.RoundedCheckBox roundedCheckBox1;
+        private LeonUI.Controls.RoundedButton roundedButton1;
+        private LeonUI.Controls.RoundedButton roundedButton2;
+        private LeonUI.Controls.RoundedButton roundedButton3;
+    }
+}
+
